ecf0234cdd7242d1f114025acad835a12b39dcad
Qbitguy <56826765+Qbitguy@users.noreply.github.com>
2019-10-28 00:39:29 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,9 +2,6 @@
 function nav_stacked_menu(array $table, $bodyTitle=null)
 {
 	$active = " class=\"active\" ";
-	if (isset($bodyTitle)):
-	 echo "<h1 align=\"center\">$bodyTitle</h1><hr>";
-	endif;
 	echo "<div class=\"row\">";
 	echo "<ul class=\" nav nav-stacked nav-pills col-md-2 menu\">\n";
 	foreach ($table as $key => $item) {		
@@ -12,6 +9,5 @@ function nav_stacked_menu(array $table, $bodyTitle=null)
 	$active=null;
 	}
 	echo "</ul>";
-	echo "</div>";
 }?>
 
