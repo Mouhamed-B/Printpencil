a6079c5ebbd296af0b72e3a13f6a828ed561f1bb
Qbitguy <56826765+Qbitguy@users.noreply.github.com>
2019-10-28 01:17:21 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,6 @@
 function nav_stacked_menu(array $table, $bodyTitle=null)
 {
 	$active = " class=\"active\" ";
-	echo "<div class=\"row\">";
 	echo "<ul class=\" nav nav-stacked nav-pills col-md-2 menu\">\n";
 	foreach ($table as $key => $item) {		
 	echo "<li$active><a href=\"#$key\" data-toggle=\"tab\">$item</a></li>\n";
